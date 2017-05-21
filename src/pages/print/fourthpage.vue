bfce5ed4b2d4541a03c9d2f33d0012b4ca861a6c
jian.lv <j.lv@gtscloud.cc>
2017-05-22 00:42:04 +0800

    实现打印数据绑定

--- src/pages/print/fourthpage.vue
+++ src/pages/print/fourthpage.vue
@@ -206,9 +206,9 @@
               <Date-picker type="date" placeholder="请选择填表日期" v-model="formItem.issuingDate"></Date-picker>
             </i-col>
             <!--<i-col span="2" style="text-align: center">-</i-col>
-                                                  <i-col span="11">
-                                                     
-                                                  </i-col>-->
+                                                    <i-col span="11">
+                                                       
+                                                    </i-col>-->
           </Row>
         </Form-item>
         <Form-item>
@@ -522,90 +522,68 @@ export default {
 
 
 
+
       LODOP.PRINT_INITA(10, 10, 762, 545, "恩施农村土地确权证");
-      LODOP.ADD_PRINT_SETUP_BKIMG("C:\\Users\\AnnieXiong\\Pictures\\打印\\44.png");
-      LODOP.ADD_PRINT_TEXT(99, 58, 44, 40, "11");
+      LODOP.ADD_PRINT_SETUP_BKIMG("src\\assets\\55.png");
+      LODOP.ADD_PRINT_TEXT(89, 438, 44, 55, "11");
       LODOP.SET_PRINT_STYLEA(0, "FontColor", "#0000FF");
-      LODOP.ADD_PRINT_TEXT(98, 109, 44, 42, "12");
-      LODOP.SET_PRINT_STYLEA(0, "FontColor", "#0000FF");
-      LODOP.ADD_PRINT_TEXT(402, 112, 50, 55, "62");
-      LODOP.SET_PRINT_STYLEA(0, "FontColor", "#0000FF");
-      LODOP.ADD_PRINT_TEXT(402, 164, 47, 55, "63");
+      LODOP.ADD_PRINT_TEXT(89, 487, 44, 57, "12");
       LODOP.SET_PRINT_STYLEA(0, "FontColor", "#0000FF");
       LODOP.ADD_PRINT_TEXT(481, 790, 14, 20, "5");
       LODOP.SET_PRINT_STYLEA(0, "FontColor", "#0000FF");
-      LODOP.ADD_PRINT_TEXT(99, 160, 44, 41, "13");
+      LODOP.ADD_PRINT_TEXT(90, 536, 44, 56, "13");
       LODOP.SET_PRINT_STYLEA(0, "FontColor", "#0000FF");
-      LODOP.ADD_PRINT_TEXT(99, 210, 44, 42, "1是");
+      LODOP.ADD_PRINT_TEXT(90, 586, 44, 57, "1是");
       LODOP.SET_PRINT_STYLEA(0, "FontColor", "#0000FF");
-      LODOP.ADD_PRINT_TEXT(156, 66, 45, 56, "21");
-      LODOP.ADD_PRINT_TEXT(156, 115, 45, 56, "22");
-      LODOP.ADD_PRINT_TEXT(156, 165, 45, 56, "23");
-      LODOP.ADD_PRINT_TEXT(155, 215, 45, 56, "2是");
-      LODOP.ADD_PRINT_TEXT(197, 285, 70, 15, "2n");
+      LODOP.ADD_PRINT_TEXT(152, 437, 45, 56, "21");
+      LODOP.ADD_PRINT_TEXT(153, 487, 45, 56, "22");
+      LODOP.ADD_PRINT_TEXT(153, 537, 45, 56, "23");
+      LODOP.ADD_PRINT_TEXT(153, 586, 45, 56, "2是");
+      LODOP.ADD_PRINT_TEXT(194, 655, 70, 15, "2n");
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(181, 285, 70, 16, "2s");
+      LODOP.ADD_PRINT_TEXT(178, 655, 70, 16, "2s");
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(136, 284, 70, 15, "1n");
+      LODOP.ADD_PRINT_TEXT(132, 654, 70, 15, "1n");
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(121, 284, 70, 15, "1s");
+      LODOP.ADD_PRINT_TEXT(117, 654, 70, 15, "1s");
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(91, 284, 70, 15, "1e");
+      LODOP.ADD_PRINT_TEXT(87, 654, 70, 15, "1e");
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(106, 284, 70, 15, "1w");
+      LODOP.ADD_PRINT_TEXT(102, 654, 70, 15, "1w");
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(153, 285, 70, 15, "2e");
+      LODOP.ADD_PRINT_TEXT(150, 655, 70, 15, "2e");
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(168, 285, 70, 14, "2w");
+      LODOP.ADD_PRINT_TEXT(165, 655, 70, 14, "2w");
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(402, 65, 45, 55, "61");
-      LODOP.SET_PRINT_STYLEA(0, "FontColor", "#0000FF");
-      LODOP.ADD_PRINT_TEXT(277, 215, 47, 55, "4是");
-      LODOP.ADD_PRINT_TEXT(277, 165, 47, 55, "43");
-      LODOP.ADD_PRINT_TEXT(277, 115, 47, 55, "42");
-      LODOP.ADD_PRINT_TEXT(276, 63, 48, 57, "41");
-      LODOP.ADD_PRINT_TEXT(217, 214, 47, 55, "3是");
-      LODOP.ADD_PRINT_TEXT(216, 166, 47, 55, "33");
-      LODOP.ADD_PRINT_TEXT(216, 116, 47, 55, "32");
-      LODOP.ADD_PRINT_TEXT(215, 64, 48, 57, "31");
-      LODOP.ADD_PRINT_TEXT(339, 214, 47, 55, "5是");
-      LODOP.ADD_PRINT_TEXT(339, 164, 47, 55, "53");
-      LODOP.ADD_PRINT_TEXT(339, 114, 47, 55, "52");
-      LODOP.ADD_PRINT_TEXT(338, 64, 48, 57, "51");
-      LODOP.ADD_PRINT_TEXT(395, 285, 70, 15, "6e");
+      LODOP.ADD_PRINT_TEXT(284, 585, 47, 55, "4是");
+      LODOP.ADD_PRINT_TEXT(284, 536, 47, 55, "43");
+      LODOP.ADD_PRINT_TEXT(284, 486, 47, 55, "42");
+      LODOP.ADD_PRINT_TEXT(283, 436, 48, 57, "41");
+      LODOP.ADD_PRINT_TEXT(220, 585, 47, 55, "3是");
+      LODOP.ADD_PRINT_TEXT(219, 537, 47, 55, "33");
+      LODOP.ADD_PRINT_TEXT(219, 486, 47, 55, "32");
+      LODOP.ADD_PRINT_TEXT(216, 436, 48, 57, "31");
+      LODOP.ADD_PRINT_TEXT(323, 655, 70, 15, "4n");
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(410, 285, 70, 14, "6w");
+      LODOP.ADD_PRINT_TEXT(307, 655, 70, 16, "4s");
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(423, 285, 70, 16, "6s");
+      LODOP.ADD_PRINT_TEXT(294, 655, 70, 14, "4w");
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(439, 285, 70, 15, "6n");
+      LODOP.ADD_PRINT_TEXT(280, 655, 70, 15, "4e");
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(378, 286, 70, 15, "5n");
+      LODOP.ADD_PRINT_TEXT(259, 654, 70, 15, "3n");
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(362, 286, 70, 16, "5s");
+      LODOP.ADD_PRINT_TEXT(243, 654, 70, 16, "3s");
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(349, 286, 70, 14, "5w");
+      LODOP.ADD_PRINT_TEXT(230, 654, 70, 14, "3w");
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(334, 286, 70, 15, "5e");
+      LODOP.ADD_PRINT_TEXT(215, 654, 70, 15, "3e");
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(317, 286, 70, 15, "4n");
-      LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(301, 286, 70, 16, "4s");
-      LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(288, 286, 70, 14, "4w");
-      LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(273, 286, 70, 15, "4e");
-      LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(258, 286, 70, 15, "3n");
-      LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(242, 286, 70, 16, "3s");
-      LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(229, 286, 70, 14, "3w");
-      LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(214, 286, 70, 15, "3e");
-      LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(402, 214, 47, 55, "6是");
-      LODOP.ADD_PRINT_TEXT(37, 158, 94, 52, "");
+      LODOP.ADD_PRINT_TEXT(489, 577, 40, 20, "2017");
+      LODOP.ADD_PRINT_TEXT(489, 631, 20, 20, "5");
+      LODOP.SET_PRINT_STYLEA(0, "AlignJustify", 1);
+      LODOP.ADD_PRINT_TEXT(489, 657, 25, 20, "22");
+
 
 
 
@@ -617,89 +595,66 @@ export default {
 
 
       LODOP.PRINT_INITA(10, 10, 762, 545, "恩施农村土地确权证");
-      LODOP.ADD_PRINT_SETUP_BKIMG("C:\\Users\\AnnieXiong\\Pictures\\打印\\44.png");
-      LODOP.ADD_PRINT_TEXT(99, 58, 44, 40, "11");
+      LODOP.ADD_PRINT_SETUP_BKIMG("C:\\Users\\AnnieXiong\\Pictures\\打印\\55.png");
+      LODOP.ADD_PRINT_TEXT(89, 438, 44, 55, this.formItem.landName);
       LODOP.SET_PRINT_STYLEA(0, "FontColor", "#0000FF");
-      LODOP.ADD_PRINT_TEXT(98, 109, 44, 42, "12");
-      LODOP.SET_PRINT_STYLEA(0, "FontColor", "#0000FF");
-      LODOP.ADD_PRINT_TEXT(402, 112, 50, 55, "62");
-      LODOP.SET_PRINT_STYLEA(0, "FontColor", "#0000FF");
-      LODOP.ADD_PRINT_TEXT(402, 164, 47, 55, "63");
+      LODOP.ADD_PRINT_TEXT(89, 487, 44, 57, this.formItem.landNO);
       LODOP.SET_PRINT_STYLEA(0, "FontColor", "#0000FF");
       LODOP.ADD_PRINT_TEXT(481, 790, 14, 20, "5");
       LODOP.SET_PRINT_STYLEA(0, "FontColor", "#0000FF");
-      LODOP.ADD_PRINT_TEXT(99, 160, 44, 41, "13");
+      LODOP.ADD_PRINT_TEXT(90, 536, 44, 56, this.formItem.realArea);
       LODOP.SET_PRINT_STYLEA(0, "FontColor", "#0000FF");
-      LODOP.ADD_PRINT_TEXT(99, 210, 44, 42, "1是");
+      LODOP.ADD_PRINT_TEXT(90, 586, 44, 57, this.formItem.ifBasefarm);
       LODOP.SET_PRINT_STYLEA(0, "FontColor", "#0000FF");
-      LODOP.ADD_PRINT_TEXT(156, 66, 45, 56, "21");
-      LODOP.ADD_PRINT_TEXT(156, 115, 45, 56, "22");
-      LODOP.ADD_PRINT_TEXT(156, 165, 45, 56, "23");
-      LODOP.ADD_PRINT_TEXT(155, 215, 45, 56, "2是");
-      LODOP.ADD_PRINT_TEXT(197, 285, 70, 15, "2n");
+      LODOP.ADD_PRINT_TEXT(152, 437, 45, 56, this.formItem.landName2);
+      LODOP.ADD_PRINT_TEXT(153, 487, 45, 56, this.formItem.landNO2);
+      LODOP.ADD_PRINT_TEXT(153, 537, 45, 56, this.formItem.realArea2);
+      LODOP.ADD_PRINT_TEXT(153, 586, 45, 56, this.formItem.ifBasefarm2);
+      LODOP.ADD_PRINT_TEXT(194, 655, 70, 15, this.formItem.north2);
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(181, 285, 70, 16, "2s");
+      LODOP.ADD_PRINT_TEXT(178, 655, 70, 16, this.formItem.south2);
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(136, 284, 70, 15, "1n");
+      LODOP.ADD_PRINT_TEXT(132, 654, 70, 15, this.formItem.north);
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(121, 284, 70, 15, "1s");
+      LODOP.ADD_PRINT_TEXT(117, 654, 70, 15, this.formItem.south);
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(91, 284, 70, 15, "1e");
+      LODOP.ADD_PRINT_TEXT(87, 654, 70, 15, this.formItem.east);
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(106, 284, 70, 15, "1w");
+      LODOP.ADD_PRINT_TEXT(102, 654, 70, 15, this.formItem.west);
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(153, 285, 70, 15, "2e");
+      LODOP.ADD_PRINT_TEXT(150, 655, 70, 15, this.formItem.east2);
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(168, 285, 70, 14, "2w");
+      LODOP.ADD_PRINT_TEXT(165, 655, 70, 14, this.formItem.west2);
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(402, 65, 45, 55, "61");
-      LODOP.SET_PRINT_STYLEA(0, "FontColor", "#0000FF");
-      LODOP.ADD_PRINT_TEXT(277, 215, 47, 55, "4是");
-      LODOP.ADD_PRINT_TEXT(277, 165, 47, 55, "43");
-      LODOP.ADD_PRINT_TEXT(277, 115, 47, 55, "42");
-      LODOP.ADD_PRINT_TEXT(276, 63, 48, 57, "41");
-      LODOP.ADD_PRINT_TEXT(217, 214, 47, 55, "3是");
-      LODOP.ADD_PRINT_TEXT(216, 166, 47, 55, "33");
-      LODOP.ADD_PRINT_TEXT(216, 116, 47, 55, "32");
-      LODOP.ADD_PRINT_TEXT(215, 64, 48, 57, "31");
-      LODOP.ADD_PRINT_TEXT(339, 214, 47, 55, "5是");
-      LODOP.ADD_PRINT_TEXT(339, 164, 47, 55, "53");
-      LODOP.ADD_PRINT_TEXT(339, 114, 47, 55, "52");
-      LODOP.ADD_PRINT_TEXT(338, 64, 48, 57, "51");
-      LODOP.ADD_PRINT_TEXT(395, 285, 70, 15, "6e");
+      LODOP.ADD_PRINT_TEXT(284, 585, 47, 55, this.formItem.ifBasefarm4);
+      LODOP.ADD_PRINT_TEXT(284, 536, 47, 55, this.formItem.realArea4);
+      LODOP.ADD_PRINT_TEXT(284, 486, 47, 55, this.formItem.landNO4);
+      LODOP.ADD_PRINT_TEXT(283, 436, 48, 57, this.formItem.landName4);
+      LODOP.ADD_PRINT_TEXT(220, 585, 47, 55, this.formItem.ifBasefarm3);
+      LODOP.ADD_PRINT_TEXT(219, 537, 47, 55, this.formItem.realArea3);
+      LODOP.ADD_PRINT_TEXT(219, 486, 47, 55, this.formItem.landNO3);
+      LODOP.ADD_PRINT_TEXT(216, 436, 48, 57, this.formItem.landName3);
+      LODOP.ADD_PRINT_TEXT(323, 655, 70, 15, this.formItem.north4);
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(410, 285, 70, 14, "6w");
+      LODOP.ADD_PRINT_TEXT(307, 655, 70, 16, this.formItem.south4);
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(423, 285, 70, 16, "6s");
+      LODOP.ADD_PRINT_TEXT(294, 655, 70, 14, this.formItem.west4);
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(439, 285, 70, 15, "6n");
+      LODOP.ADD_PRINT_TEXT(280, 655, 70, 15, this.formItem.east4);
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(378, 286, 70, 15, "5n");
+      LODOP.ADD_PRINT_TEXT(259, 654, 70, 15, this.formItem.north3);
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(362, 286, 70, 16, "5s");
+      LODOP.ADD_PRINT_TEXT(243, 654, 70, 16, this.formItem.south3);
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(349, 286, 70, 14, "5w");
+      LODOP.ADD_PRINT_TEXT(230, 654, 70, 14, this.formItem.west3);
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(334, 286, 70, 15, "5e");
+      LODOP.ADD_PRINT_TEXT(215, 654, 70, 15, this.formItem.east3);
       LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(317, 286, 70, 15, "4n");
-      LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(301, 286, 70, 16, "4s");
-      LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(288, 286, 70, 14, "4w");
-      LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(273, 286, 70, 15, "4e");
-      LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(258, 286, 70, 15, "3n");
-      LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(242, 286, 70, 16, "3s");
-      LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(229, 286, 70, 14, "3w");
-      LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(214, 286, 70, 15, "3e");
-      LODOP.SET_PRINT_STYLEA(0, "FontSize", 7);
-      LODOP.ADD_PRINT_TEXT(402, 214, 47, 55, "6是");
-      LODOP.ADD_PRINT_TEXT(37, 158, 94, 52, "");
+      LODOP.ADD_PRINT_TEXT(489, 577, 40, 20, this.formItem.issuingDate.getFullYear());
+      LODOP.ADD_PRINT_TEXT(489, 631, 20, 20, this.formItem.issuingDate.getMonth()+1);
+      LODOP.SET_PRINT_STYLEA(0, "AlignJustify", 1);
+      LODOP.ADD_PRINT_TEXT(489, 657, 25, 20, this.formItem.issuingDate.getDate());
+
 
 
 
